135161a1512c1182ea7db65b584863fa862e1b72
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-09-24 05:40:40 +0800

    init repo

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from kivy.clock import Clock
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.toolbar import MDTopAppBar
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import BoxLayout
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.label import MDLabel
 
-class Main(MDApp):
+class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def build(self):
-        self.mainScreen = MDScreen()
-        # Top Navigation Toolbar
-
-        # render main screen
+        self.mainScreen = MDBoxLayout()
+        self.topNaviBar = MDTopAppBar(
+            id="topNaviBar",
+            title="MOODOX",
+            pos_hint={'top': 1},
+            left_action_items=[["head-dots-horizontal-outline"]]
+        )
+        self.mainScreen.add_widget(self.topNaviBar)
         return self.mainScreen
 
-Main().run()
+MainApp().run()
