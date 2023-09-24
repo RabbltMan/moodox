1834ae3a7df314a44a7cb28f742166c8d38fffc9
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-09-24 15:53:38 +0800

    init

--- bin/components.py
+++ bin/components.py
@@ -1,13 +1,8 @@
 from kivymd.uix.toolbar import MDTopAppBar
+from main import MainApp
 
-class AppBars(object):
-    def __init__(self) -> None:
-        self.topNaviBar = MDTopAppBar(
-            id="topNaviBar",
-            title="MOODOX",
-            pos_hint={'top': 1},
-            left_action_items=[
-                ["head-dots-horizontal-outline", lambda _: self.switchScreen()]
-            ]
-        )
+MainTopNaviBar = MDTopAppBar(id="topNaviBar", 
+                         title="MOODOX", 
+                         pos_hint={'top': 1}, 
+                         left_action_items=[["head-dots-horizontal-outline", lambda _: MainApp.switchScreen()]])
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.toolbar import MDTopAppBar
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
 from bin.components import *
@@ -8,29 +6,19 @@ class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def switchScreen(self, target):
-        print(f"Switching to {target}")
-        self.screenManager.transition.direction = 'right'
-        self.screenManager.current = target
+    def switchScreen(self):
+        pass
 
     def build(self):
         # render screen manager
         self.screenManager = ScreenManager()
         # render main screen
         self.mainScreen = Screen(name="main")
-        self.topNaviBar = AppBars().topNaviBar
-        self.topNaviBar.left_action_items = [
-            ["head-dots-horizontal-outline", lambda _: self.switchScreen("bs")]
-        ]
-        self.mainScreen.add_widget(self.topNaviBar)
+        self.mainScreen.add_widget(MainTopNaviBar)
         self.screenManager.add_widget(self.mainScreen)
         # render brainstorm screen
         self.bsScreen = Screen(name="bs")
-        self.topNaviBar = AppBars().topNaviBar
-        self.topNaviBar.left_action_items = [
-            ["head-dots-horizontal-outline", lambda _: self.switchScreen("main")]
-        ]
-        self.bsScreen.add_widget(self.topNaviBar)
+        self.bsScreen.add_widget(MainTopNaviBar)
         self.screenManager.add_widget(self.bsScreen)
         # screen manager state
         self.screenManager.current = "main"
