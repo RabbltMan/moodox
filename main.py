245c450f3957fddc27080643e4cc94fd57233662
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-09-29 20:14:59 +0800

    top app bar and content background

--- main.py
+++ main.py
@@ -6,20 +6,16 @@ class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def switchScreen(self):
-        pass
-
     def build(self):
+        components = Components()
         # render screen manager
         self.screenManager = ScreenManager()
         # render main screen
         self.mainScreen = Screen(name="main")
-        self.mainScreen.add_widget(MainTopNaviBar)
+        self.mainScreen.add_widget(components.MainTopNaviBar, index=1)
+        self.mainScreen.add_widget(components.MainContent, index=2)
+        self.mainScreen.add_widget(components.logo)
         self.screenManager.add_widget(self.mainScreen)
-        # render brainstorm screen
-        self.bsScreen = Screen(name="bs")
-        self.bsScreen.add_widget(MainTopNaviBar)
-        self.screenManager.add_widget(self.bsScreen)
         # screen manager state
         self.screenManager.current = "main"
         return self.screenManager
--- partials/components.py
+++ partials/components.py
@@ -1,8 +1,32 @@
 from kivymd.uix.toolbar import MDTopAppBar
-from main import MainApp
+from kivymd.uix.fitimage import FitImage
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.image import Image
 
-MainTopNaviBar = MDTopAppBar(id="topNaviBar", 
-                         title="MOODOX", 
-                         pos_hint={'top': 1}, 
-                         left_action_items=[["head-dots-horizontal-outline", lambda _: MainApp.switchScreen()]])
+class Components(object):
+    def __init__(self) -> None:
+        self.logo = Image(
+            source='./src/logo.png',
+            size_hint=(0.4, 0.06),
+            opacity=0.65,
+            pos_hint={"center_x": .5, "center_y": .93},
+        )
+        self.MainTopNaviBar = MDTopAppBar(
+            id="top_navi",  
+            pos_hint={'top': 1}, 
+            elevation=0,
+            md_bg_color=(99/255, 235/255, 233/255, 0),
+            specific_text_color=(50/255, 50/255, 50/255, 1),
+            left_action_items=[["head-dots-horizontal-outline"]]
+        )
+
+        self.MainContent = MDBoxLayout(
+            md_bg_color=(216/255, 185/255, 156/255, 1),
+        )
+        self.MainBackground = FitImage(
+            source="./src/bg.jpg",
+            pos_hint={"top": 1},
+            opacity=0.5
+        )
+        self.MainContent.add_widget(self.MainBackground)
 
