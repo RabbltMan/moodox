c646270a2a0d779f700b966fa7bfd31f7adae12c
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-10-03 02:33:32 +0800

    implement date display on card

--- partials/components.py
+++ partials/components.py
@@ -1,18 +1,20 @@
-from os import listdir
 from kivymd.uix.fitimage import FitImage
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.card import MDCard
-from kivymd.uix.fitimage import FitImage
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivymd.uix.gridlayout import MDGridLayout
+from kivy.uix.label import Label
 from kivymd.uix.button import MDFloatingActionButton
 from kivy.uix.image import Image
 from random import choice
+from os import listdir
+from datetime import date
 
 class Components(object):
     def __init__(self) -> None:
         self.MainFloatingHeadButton = MDFloatingActionButton(
             id='top_head',
             icon="head-dots-horizontal-outline",
-            pos_hint={"right": 0.95, "y": 0.02},
+            pos_hint={"right": 0.990, "y": 0.008},
             md_bg_color=(0.5, 0.5, 0.5, 0),
             icon_color=(0.3, 0.4, 0.7, 1),
             icon_size="32sp",
@@ -37,10 +39,10 @@ class Components(object):
         )
         self.MainBackgroundFilter.add_widget(self.BackgroundImage)
 
-        self.DailyCard = MDCard(
+        self.DailyCard = MDRelativeLayout(
             id='daily_card',
-            radius=24,
-            md_bg_color=(0.3, 0.3, 0.3, 0.5),
+            radius=(24, 24, 24, 24),
+            md_bg_color=(0.3, 0.3, 0.3, 0.2),
             pos_hint={"center_x": .5, "center_y": .5},
             size_hint=(0.9, 0.77),
         )
@@ -52,4 +54,38 @@ class Components(object):
             opacity=0.77,
             radius=(24, 24, 0, 0),
         )
+        self.dateSection = MDGridLayout(
+            pos_hint={"x": 0, "y": 0.8},
+            rows=3,
+            cols=1,
+            md_bg_color=(1, 1, 1, 0.8),
+            size_hint=(0.25, 0.2),
+            radius=(24, 0, 20, 0)
+        )
+        self.monthLabel = Label(
+            text=str(date.today().month),
+            pos_hint={"x": 0.75},
+            padding_x=0.3,
+            color=(1, 0.3, 0, 1),
+            font_size="27sp",
+            font_name="./src/DottedSong.ttf"
+        )
+        self.dayLabel = Label(
+            text=str(date.today().day),
+            padding_x=0.3,
+            color=(0, 0.3, 1, 1),
+            font_size="27sp",
+            font_name="./src/DottedSong.ttf"
+        )
+        self.weekDayLabel = Label(
+            text=["MON","TUE","WED","THU","FRI","SAT","SUN"][date.today().weekday()],
+            padding_x=0.3,
+            color=(0, 0, 0, 1),
+            font_size="16sp",
+            font_name="./src/DottedSong.ttf"
+        )
         self.DailyCard.add_widget(self.cardImage)
+        self.dateSection.add_widget(self.monthLabel)
+        self.dateSection.add_widget(self.weekDayLabel)
+        self.dateSection.add_widget(self.dayLabel)
+        self.DailyCard.add_widget(self.dateSection)
